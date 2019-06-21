ba4f377656d3b243da2b172d99253cf67c9761d4
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-06-21 13:18:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -32,9 +32,7 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
-  if source_array == TRUE
-    source_array.reduce
-  end
+  source_array.all?
 end
   
 def reduce_to_any_true(source_array)
